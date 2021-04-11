6bbfb94748ee23cae93541ee7cc3f164929dd68c
bk0110 <1102bhawna@gmail.com>
2021-04-12 00:18:23 +0530

    gauge added

---
+++ bot/static/bot/gauge.js
@@ -0,0 +1,15 @@
+function setGaugeValue(value) {
+    var gauge = document.querySelector(".gauge");
+    if (value < 0 || value > 1) {
+    return;
+  }
+
+  gauge.querySelector(".gauge__fill").style.transform = `rotate(${
+    value / 2
+  }turn)`;
+  gauge.querySelector(".gauge__cover").textContent = `${Math.round(
+    value * 100
+  )}kmph`;
+}
+
+
